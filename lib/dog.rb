58264f508944292a62622c81c81b16ac8f384a43
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-09 22:14:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,3 @@
 class Dog 
-  def initialize(name)
-    @name = name 
-  end 
-  
-  def name 
-    @name
-    # you are "getting" the name from above 
-  end 
-  
-  def name=(new_name)
-    @name = new_name
-  end
   
 end 
